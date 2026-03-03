3be4d294bb6e3a539b0d5c2787350a54046f6678
Jingjing Bao <JBao6@slb.com>
2026-03-03 09:23:46 +0800

    add

--- azure-ai-search/search-101.py
+++ azure-ai-search/search-101.py
@@ -1,10 +1,20 @@
+import os
+
 from azure.identity import DefaultAzureCredential
+from azure.core.credentials import AzureKeyCredential
 from azure.search.documents.indexes import SearchIndexClient
 import json
 
-service_endpoint = "https://new-search-8161.search.windows.net"
-credential = DefaultAzureCredential()
-client = SearchIndexClient(endpoint = service_endpoint, credential = credential)
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AZURE_SEARCH_ENDPOINT = os.getenv("AZURE_SEARCH_SERVICE_ENDPOINT")
+AZURE_SEARCH_INDEX_NAME = os.getenv("AZURE_SEARCH_INDEX_NAME")
+AZURE_SEARCH_ADMIN_KEY = os.getenv("AZURE_SEARCH_ADMIN_KEY")
+
+azure_credential=AzureKeyCredential(AZURE_SEARCH_ADMIN_KEY)
+client = SearchIndexClient(endpoint = AZURE_SEARCH_ENDPOINT, credential = azure_credential)
 
 # List existing indexes
 indexes = client.list_indexes()
@@ -12,4 +22,3 @@ indexes = client.list_indexes()
 for index in indexes:
    index_dict = index.as_dict()
    print("Index Name:", index_dict['name'])
-   print(json.dumps(index_dict, indent = 2))
